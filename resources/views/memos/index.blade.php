e8f8528e15b1e725c721b19fc6def380cd3cfe4a
kazu-taka <k.takahashi@nowall.co.jp>
2024-09-12 08:34:51 +0900

    登録機能の作成

--- resources/views/memos/index.blade.php
+++ resources/views/memos/index.blade.php
@@ -17,6 +17,8 @@
             <li><a href="{{ route('memos.show', $memo) }}">{{ $memo->title }}</a></li>
         @endforeach
     </ul>
+
+    <button onclick='location.href="{{ route("memos.create") }}"'>登録する</button>
 </body>
 
 </html>
