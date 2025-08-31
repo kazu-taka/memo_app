3e7c4b7cc0a5802a672d654969b70dccd58cd301
kazu-taka <k.takahashi@next-revolution.net>
2025-08-31 14:31:44 +0900

    削除機能の完成

--- resources/views/livewire/memos/show.blade.php
+++ resources/views/livewire/memos/show.blade.php
@@ -9,6 +9,10 @@ state(['memo' => fn(Memo $memo) => $memo]);
 $edit = function() {
     return redirect()->route('memos.edit', $this->memo);
 };
+$destroy = function() {
+    $this->memo->delete();
+    return redirect()->route('memos.index');
+};
 ?>
 
 <div>
@@ -17,4 +21,5 @@ $edit = function() {
     <p>{!! nl2br(($memo->body)) !!}</p>
 
     <button wire:click="edit">編集する</button>
+    <button wire:click="destroy" wire:confirm="本当に削除しますか？">削除する</button>
 </div>
