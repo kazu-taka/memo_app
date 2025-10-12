d30e4288db9dacad81da6f67bbe596e1a1a66021
kazu-taka <k.takahashi@next-revolution.net>
2025-10-12 09:33:27 +0900

    バリデーション実装

--- resources/views/livewire/memos/create.blade.php
+++ resources/views/livewire/memos/create.blade.php
@@ -1,9 +1,14 @@
 <?php
 
-use function Livewire\Volt\{state};
+use function Livewire\Volt\{state, rules};
 use App\Models\Memo;
 
 state(['title', 'body']);
+
+rules([
+    'title' => 'required|string|max:50',
+    'body' => 'required|string|max:2000',
+]);
 
 // メモを保存する関数
 $store = function () {
@@ -12,6 +17,7 @@ $store = function () {
     //     'title' => $this->title,
     //     'body' => $this->body,
     // ]);
+    $this->validate();
     Memo::create($this->all());
     // 一覧ページにリダイレクト
     return redirect()->route('memos.index');
@@ -25,11 +31,19 @@ $store = function () {
 
     <form wire:submit="store">
         <p>
-            <label for="title">タイトル</label><br>
+            <label for="title">タイトル</label>
+            @error('title')
+                <span class="error">({{ $message }})</span>
+            @enderror
+            <br>
             <input type="text" wire:model="title" id="title">
         </p>
         <p>
-            <label for="body">本文</label><br>
+            <label for="body">本文</label>
+            @error('body')
+                <span class="error">({{ $message }})</span>
+            @enderror
+            <br>
             <textarea wire:model="body" id="body"></textarea>
         </p>
         <button type="submit">登録</button>
